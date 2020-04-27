dba3390a4fe226bf5428cce1fbabb4edb6387f3a
Rojin Abraham <rojin.abraham.jb@gmail.com>
2020-04-27 13:09:42 +0530

    Updates

---
+++ precache-manifest.19eb9fe1e2695fc73c0a8e432c174847.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cc7a2d6a590decc003488c71237a19",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "f378dc8427f41b2fdce1",
+    "url": "/FaceRecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "3f44e8daec1986090ccb",
+    "url": "/FaceRecognition/static/css/main.25e9744c.chunk.css"
+  },
+  {
+    "revision": "f378dc8427f41b2fdce1",
+    "url": "/FaceRecognition/static/js/2.7ac74c1f.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/FaceRecognition/static/js/2.7ac74c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f44e8daec1986090ccb",
+    "url": "/FaceRecognition/static/js/main.1e7c3c7c.chunk.js"
+  },
+  {
+    "revision": "b8098bcadd344e1143a1",
+    "url": "/FaceRecognition/static/js/runtime-main.d34e579d.js"
+  },
+  {
+    "revision": "c32baf0426c1b4b33b18d3791bcc0bad",
+    "url": "/FaceRecognition/static/media/FaceLogo11.c32baf04.png"
+  }
+]);
